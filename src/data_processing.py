f3b6c181491cad49dfaae40525497c50faf24fcf
Thabo-Mathyuz <85599228+Thabo-web@users.noreply.github.com>
2026-08-01 01:34:40 +0000

    added edited changes

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,12 +1,12 @@
-%matplotlib inline
 import pandas as pd
 from pathlib import Path
 from typing import Optional 
 import matplotlib.pyplot as plt
 import seaborn as sns
 from src.data_loader import load_data
+##PYTHONPATH=. python3 src/data_processing.py
 
 df = load_data()
 
-sns.regplot(x='independent_variable', y='dependent_variable', data=df)
+sns.regplot(x='Country name', y='2010', data=df)
 plt.show()
